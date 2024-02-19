b9c0c5b7f446c9b377b77aa22ff1e47fec7e8efb
Tom Lorius <tomlorius.mail@gmail.com>
2024-02-19 21:14:26 +0100

    Started setup of navbar

---
+++ src/app/ui/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="flex flex-row gap-30 justify-around">
+      <h3>Tom Lorius</h3>
+      <div>Projects</div>
+      <div>About</div>
+      <div>Contact</div>
+    </div>
+  );
+};
+
+export default Navbar;
