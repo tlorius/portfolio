77f22abc8bfad7913e960be4232e15bbebbef6f5
Tom Lorius <tomlorius.mail@gmail.com>
2024-02-21 21:34:44 +0100

    feat: base form done
    removed readme boilerplate and added form to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Contact() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log({ email, message });
+  };
   return (
     <>
       <h1>Contactpage</h1>
+      <form
+        className="grid grid-cols-1 gap-4"
+        onSubmit={(event) => handleSubmit(event)}
+      >
+        <label>
+          Email
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            className="border border-solid border-black"
+          />
+        </label>
+        <label>
+          Message
+          <input
+            type="text"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            className="border border-solid border-black"
+          />
+        </label>
+        <input
+          type="submit"
+          value="Send Message"
+          className="border border-solid border-black w-fit"
+        />
+      </form>
     </>
   );
 }
